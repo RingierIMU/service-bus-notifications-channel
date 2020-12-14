66a68db2a36bfda4bdc56861b8756bad25e71652
datashaman <marlinf@datashaman.com>
2020-12-14 13:26:38 +0200

    BUS-814 Possibly fix auth issue (#26)

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -4,6 +4,7 @@ namespace Ringierimu\ServiceBusNotificationsChannel\Exceptions;
 
 use Exception;
 use Illuminate\Support\Facades\Log;
+use Psr\Http\Message\ResponseInterface;
 use Throwable;
 
 /**
@@ -18,10 +19,40 @@ class CouldNotSendNotification extends Exception
      */
     public static function authFailed(Throwable $exception)
     {
-        Log::error('Could not get an auth token from the server: ' . $exception->getMessage(), ['tag' => 'ServiceBus']);
-        Log::error($exception->getTraceAsString(), ['tag' => 'ServiceBus']);
+        Log::error(
+            'Could not get an auth token from the server',
+            [
+                'exception' => $exception,
+                'tags' => [
+                    'service-bus',
+                ],
+            ]
+        );
 
         return new static('Could not get an auth token from the server: ' . $exception->getMessage());
+    }
+
+    /**
+     * @param ResponseInterface $response
+     *
+     * @return CouldNotSendNotification
+     */
+    public static function loginFailed(ResponseInterface $response)
+    {
+        Log::error(
+            'Something went wrong logging in',
+            [
+                'response' => [
+                    'statusCode' => $response->getStatusCode(),
+                    'body' => (string) $response->getBody(),
+                ],
+                'tags' => [
+                    'service-bus',
+                ],
+            ]
+        );
+
+        return new static('Something went wrong logging in');
     }
 
     /**
@@ -31,8 +62,15 @@ class CouldNotSendNotification extends Exception
      */
     public static function requestFailed(Throwable $exception)
     {
-        Log::error('Something went wrong logging the event: ' . $exception->getMessage(), ['tag' => 'ServiceBus']);
-        Log::error($exception->getTraceAsString(), ['tag' => 'ServiceBus']);
+        Log::error(
+            'Something went wrong logging the event',
+            [
+                'exception' => $exception,
+                'tags' => [
+                    'service-bus',
+                ],
+            ]
+        );
 
         return new static('Something went wrong logging the event: ' . $exception->getMessage());
     }
